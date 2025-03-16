163d8d82e8155776618004daf72a50d88032178a
Arsalan Yaldram <arsalanyaldram0211@outlook.com>
2025-03-16 16:11:10 +0530

    feat: refactor add() function, all tests passing.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 export function add(numbers) {
   if (numbers === "") return 0;
 
-  const splitNumbers = numbers.split(",");
-  let sum = 0;
-
-  for (let i = 0; i < splitNumbers.length; i++) {
-    sum += parseInt(splitNumbers[i], 10);
-  }
-
-  return sum;
+  return numbers
+  .split(",")
+  .map(Number)
+  .reduce((sum, num) => sum + num, 0);
 }
