aab51dba73dc96bb81faa6cddf92ac70d07d35d8
Arsalan Yaldram <arsalanyaldram0211@outlook.com>
2025-03-21 19:50:31 +0530

    feat: added a form to test the add function.

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import { add } from "./index";
+
 const App = () => {
-  return <h1>Hello from Vite + React!</h1>;
+  const [numberInput, setNumberInput] = React.useState("");
+  const [result, setResult] = React.useState(0);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+ 
+    // Replace all occurrences of '\n' with actual newline characters
+    const processedInput = numberInput.replace(/\\n/g, '\n');
+ 
+    const result = add(processedInput);
+    setResult(result);
+  };
+
+  return (
+    <div>
+      <form
+        style={{ display: "flex", flexDirection: "column", gap: "1rem" }}
+        onSubmit={handleSubmit}
+      >
+        <input
+          type="text"
+          value={numberInput}
+          onChange={(e) => setNumberInput(e.target.value)}
+        />
+        <button>Calculate</button>
+      </form>
+      <p>Result: {result}</p>
+    </div>
+  );
 };
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
